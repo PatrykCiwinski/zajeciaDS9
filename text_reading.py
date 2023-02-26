76f2bc12fa33f0504f30202f65b1a88d6ec73271
Patryk <patryk.ciwinski@gmail.com>
2023-02-26 11:33:12 +0100

    list_comprehension in file reading

--- text_reading.py
+++ text_reading.py
@@ -1,11 +1,16 @@
 name = "file.txt"
 with open(name) as fname:
     lines=fname.readlines()
-
 # for line in lines:
 #     print(line, end="")
 
 # the same
+#
+# for line in lines:
+#     print(line.rstrip())
 
-for line in lines:
-    print(line.rstrip())
+
+lst =[row for row in lines]
+
+for l in lst:
+    print(l.strip())
